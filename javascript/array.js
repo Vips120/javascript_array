c569c70d896ddb5ad577495aa9f0cc285ec5fbb9
Vips120 <vipulsingh0000@gmail.com>
2021-07-05 15:24:55 +0530

    first lecture of javascript array

---
+++ javascript/array.js
@@ -0,0 +1,9 @@
+let username = ['raj', 'rakesh', 'john', 'emma', 'mak'];  // array
+// console.log(username[0]);
+// console.log(username[1])
+// console.log(username[2])
+// console.log(username[3])
+// console.log(username[4])
+
+let employee = ['raj', 30, true, 'playerzpot'];
+console.log(`employee name is ${employee[0]} and age ${employee[1]} is present ${employee[2]} and company name is ${employee[3]}`)
